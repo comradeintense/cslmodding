4886b286176ac0ab9a99c3f40dbf7ea40fff36c8
comradeintense <in10se_SL@yahoo.com>
2023-11-23 13:17:19 +0200

    first commit

---
+++ Tips.md
@@ -0,0 +1,14 @@
+colors:
+bg-lighter: #282B2F
+bg-darker: #212326
+
+text-headings: #bfced6
+paragraphs: #909da7
+nav-items: #a8b6bf
+nav-titles: #7c868f
+
+hover: #45a3e6
+links: #7aaacc
+
+highlight-orange: #b3937d
+highlight-green: #8fa68d
---
+++ tailwind.config.mjs
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+export default {
+	content: [
+		'./pages/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx}',
+		'./components/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx}',
+		'./app/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx}',
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx}',
+	],
+	theme: {
+		screens: {
+			sm: '580px',
+			md: '768px',
+			lg: '992px',
+			xl: '1200px',
+			'2xl': '1440px',
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1.5rem',
+				sm: '1.5rem',
+				md: '2rem',
+				lg: '3rem',
+				xl: '3.625rem',
+				'2xl': '5rem',
+			},
+		},
+		extend: {
+			fontFamily: {
+				sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
+};
