532d18b3afbc807a58122f5b2034b59d20669019
comradeintense <in10se_SL@yahoo.com>
2023-12-07 14:56:52 +0200

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
-import partytown from "@astrojs/partytown";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: true
-  }), partytown()]
+	integrations: [
+		tailwind({
+			applyBaseStyles: true,
+		}),
+	],
 });
